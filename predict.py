305c0d25ed3ea38a8983ab98dcced423b69a8575
Zr <zrendless@foxmail.com>
2024-04-02 15:08:13 +0800

    feat: 检测时使用自定义的模型

--- predict.py
+++ predict.py
@@ -4,10 +4,14 @@ from PIL import Image
 from siamese import Siamese
 
 if __name__ == "__main__":
-    model = Siamese()
+    # 默认的path是model_data/word_compare.pth
+    # model = Siamese(model_path="model_data/word_compare.pth")
+    model = Siamese(model_path="logs/best_epoch_weights.pth")
 
-    image_1 = Image.open(R"F:\Project\DeepLearning\Game\material\dataset\temp\0.JPG")
-    image_2 = Image.open(R"F:\Project\DeepLearning\Game\material\dataset\temp\2.JPG")
+    # image_1 = Image.open(R"F:\Project\DeepLearning\Game\material\dataset\temp\12.JPG")
+    image_1 = Image.open(R"F:\Project\DeepLearning\Siamese\material\mixImg\本-146-5.png")
+    # image_2 = Image.open(R"F:\Project\DeepLearning\Game\material\dataset\temp\10.JPG")
+    image_2 = Image.open(R"F:\Project\DeepLearning\Siamese\material\mixImg\本-59-0.png")
 
     probability = model.detect_image(image_1, image_2)
     print(probability)
